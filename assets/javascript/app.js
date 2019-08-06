9485a32baf16a35d9454c66493c9792b325c1625
Esteban Morell <estebanmorellb@gmail.com>
2019-08-06 17:06:38 -0500

    added user, progress, progress.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyChhfkmuIxDTD5w2c-ZnzMrxdAEVSuJQqw",
+  authDomain: "proyect-1-52d99.firebaseapp.com",
+  databaseURL: "https://proyect-1-52d99.firebaseio.com",
+  projectId: "proyect-1-52d99",
+  storageBucket: "",
+  messagingSenderId: "550696355872",
+  appId: "1:550696355872:web:b5626b42359e8d42"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 var database = firebase.database();
 
 //Game object has all game vars and funtions
@@ -50,24 +63,79 @@ var Game = {
 
   ////every time user win > send the learned think to the database
   AddProgress(where, value) {
-    database.ref(this.userName + "/" + where).once("value", function(snapshot) {
+    database.ref(this.userKey + "/" + where).once("value", function(snapshot) {
+      var found = false;
       var data = snapshot.val();
-      if (snapshot.val() !== null) {
-        console.log("no es null");
+      for (var key in data) {
+        if (data[key] === value) {
+          found = true;
+          break;
+        }
       }
-      console.log(data);
+      if (!found) {
+        database.ref(Game.userKey + "/" + where).push(value);
+      }
     });
+  },
+
+  ////filling progress.html
+  FillingProgressTables(data, theme) {
+    var table;
+    if (theme === "animals") {
+      table = $("#tbody1");
+    } else {
+      if (theme === "colors") {
+        table = $("#tbody2");
+      } else {
+        table = $("#tbody3");
+      }
+    }
+
+    table.empty();
+    if (data === null) {
+      var newRow = $("<tr>").append($("<td>").text("No Progress Yet"));
+      table.append(newRow);
+    } else {
+      for (var key in data) {
+        var newRow = $("<tr>").append($("<td>").text(data[key]));
+        table.append(newRow);
+      }
+    }
   }
   ////////Here add game  funtions***************
 };
 
 $(document).ready(function() {
-  //   Game.userName="vita";
+  Game.userKey = "-LlcLojSZqZc--9lQThG"; //////delete, only for test**********
 
-  //   database.ref("vita").collection("colors");
+  //////getting the animals progress
+  database.ref(Game.userKey + "/animals").on("value", function(snapshot) {
+    data = snapshot.val();
+    Game.FillingProgressTables(data, "animals");
+  });
 
-  //   Game.AddProgress("colors","red");
-  // database.ref('vita/hgvghv').push("ppppppp");
-  var a = Game.AddUser("Gordon");
-  //   console.log(a);
+  //////getting the colors progress
+  database.ref(Game.userKey + "/colors").on("value", function(snapshot) {
+    data = snapshot.val();
+    Game.FillingProgressTables(data, "colors");
+  });
+
+  //////getting the numbers progress
+  database.ref(Game.userKey + "/numbers").on("value", function(snapshot) {
+    data = snapshot.val();
+    Game.FillingProgressTables(data, "numbers");
+  });
+
+
+
+
+
+
+
+  // Game.AddUser("Gordon");////************
+
+  $(".card-header").on("click", function() {
+    //////delete, only for test******
+    Game.AddProgress("numbers", "seven"); //////delete, only for test********
+  }); //////delete, only for test*******
 });
